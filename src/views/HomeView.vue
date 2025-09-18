9dd287f2d52e1ee5ab21798f9e64cc661a6e34b3
Holger Drewes <Holger.Drewes@gmail.com>
2025-09-18 10:10:17 +0200

    Rename TheWelcome -> HomeIntro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeIntro from '@/components/HomeIntro.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomeIntro />
   </main>
 </template>
