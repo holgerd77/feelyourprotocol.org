10514a1c9aef6c137493854addef8e8d0bb332c1
Holger Drewes <Holger.Drewes@gmail.com>
2025-09-25 14:22:20 +0200

    Add E2E Tests (#1)
    * Renamed e2e example -> home tests
    
    * Add basic e2e tests for structure and home page
    
    * Add simple e2e workflow
    
    * Another CI test
    
    * Another revised workflow
    
    * Next try
    
    * And another one

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -5,12 +5,12 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-2 pt-2 border-t-2 border-blue-300 mt-10">
-    <h1 class="font-mono text-xs">{{ msg }}</h1>
+  <footer class="grid grid-cols-2 pt-2 border-t-2 border-blue-300 mt-10">
+    <h3 class="font-mono text-xs">{{ msg }}</h3>
 
     <h3 class="font-mono text-xs text-right">
       <a href="https://vite.dev/" target="_blank" rel="noopener">Imprint</a>
       <a href="https://vuejs.org/" target="_blank" rel="noopener" class="ml-3">GitHub</a>
     </h3>
-  </div>
+  </footer>
 </template>
