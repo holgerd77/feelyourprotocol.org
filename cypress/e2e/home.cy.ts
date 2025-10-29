44f483799ac710e4bf16ae756a7f7b6bf0a79e66
Holger Drewes <Holger.Drewes@gmail.com>
2025-10-29 17:40:56 +0100

    Fix tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,16 +4,10 @@ describe('Home and Structure Tests', () => {
   it('renders the core site components correctly', () => {
     cy.visit('/')
 
-    cy.contains('h1', 'feelyourprotocol.org')
+    cy.contains('h1', 'feelyourprotocol')
 
     cy.get('nav').get('a').contains('Fusaka')
 
     cy.get('footer').get('a').contains('Imprint')
-  })
-
-  it('displays the key components of the home page', () => {
-    cy.visit('/')
-
-    cy.get('main').contains('h3', 'Hello. You have found it!')
   })
 })
